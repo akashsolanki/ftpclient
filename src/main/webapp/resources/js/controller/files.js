4fa7c1e0fd81344adbcfecc73c7d8109dce2d614
akashsolanki <akash.solankey@gmail.com>
2016-05-10 13:41:46 +0530

    File action changes

--- src/main/webapp/resources/js/controller/files.js
+++ src/main/webapp/resources/js/controller/files.js
@@ -1,7 +1,8 @@
 'use strict';
 	angularApp.controller('fileCtrl',function($scope,$http,$rootScope) {
 		$scope.selectedFolder = [];
-		
+		$scope.foldername = "";
+		$scope.deleteAction = false;
 		$scope.getFolderList = function(){
 			$http.get('./folder/folderlist').success(
 					function(data) {
@@ -10,21 +11,14 @@
 				console.log("error: " + data);
 			});
 		};
-		$scope.createFolder = function(){
-			$http.post('./folder/create/testfolder',$scope.selectedFolder).success(
-					function(data) {
-						init();
-					}).error(function(data) {
-				console.log("error: " + data);
-			});
-		};
-		$scope.rename = function(){
-			$http.post('./folder/rename/newfolder',$scope.selectedFolder).success(
-					function(data) {
-						init();
-					}).error(function(data) {
-				console.log("error: " + data);
-			});
+		$scope.folderActions = function(foldername,action){
+			$scope.foldername = "";
+				$http.post('./folder/'+action+'/'+foldername,$scope.selectedFolder).success(
+						function(data) {
+							init();
+						}).error(function(data) {
+					console.log("error: " + data);
+				});
 		};
 		var init = function() {
 			$scope.getFolderList();
@@ -34,4 +28,12 @@
 			    nodeChildren: "children",
 			    multiSelection: false
 			};
+		
+		   $scope.showModal = false;
+		    $scope.buttonClicked = "";
+		    $scope.toggleModal = function(btnClicked,action){
+		        $scope.buttonClicked = btnClicked;
+		        $scope.action = action;
+		        $scope.showModal = !$scope.showModal;
+		    };
 		});
