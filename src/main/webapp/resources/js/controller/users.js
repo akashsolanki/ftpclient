a6edb9525928a59a8f89f6a700b5a665409b2204
subrahmanyam yepuri <subbdev@gmail.com>
2016-05-11 18:52:18 +0530

    as

--- src/main/webapp/resources/js/controller/users.js
+++ src/main/webapp/resources/js/controller/users.js
@@ -2,7 +2,8 @@
 	angularApp.controller('userCtrl',function($scope,$http,$rootScope) {
 		console.log('check');
 		$scope.users = [];
-		
+		$scope.editUserFlag = false;
+		$scope.afterUpdate = false;
 		$scope.getUsers = function(pageIndex){
 			$http.get('./user/list').success(
 					function(data) {
@@ -13,6 +14,52 @@
 		};
 		var init = function() {
 			$scope.getUsers($scope.currentPageIndex);
+			
+			$scope.editUser = function(username){
+				$scope.editUserFlag = true;
+				$scope.user = "";
+				$scope.roles = "";
+				
+				$scope.getUser = function(username){
+					$http.get('./user/get/'+username).success(
+							function(data) {
+								$scope.user = data;
+							}).error(function(data) {
+						console.log("error: " + data);
+					});
+				};
+				$scope.getRoles = function(){
+					$http.get('./user/roles/list').success(
+							function(data) {
+								$scope.roles = data;
+							}).error(function(data) {
+						console.log("error: " + data);
+					});
+				};
+				$scope.update = function(){ 
+					$scope.editUserFlag = false;
+					$("#alertDiv").show();
+					$scope.afterUpdate = true;
+					$http.post('./user/edit',$scope.user).success(
+							function(data) {
+								if(data.isSuccess)
+									init();
+								
+							}).error(function(data) {
+						console.log("error: " + data);
+					});
+					 window.setTimeout(function() {$scope.afterUpdate = false;$("#alertDiv").hide();}, 3000);
+				};
+				$scope.hide = function(){ $("#alertDiv").hide();
+				};
+				var init = function() {
+					$scope.getUser(username);
+					$scope.getRoles();
+				};
+				
+				init();
+				
+			};
 		};
 		$scope.deleteUser = function(username){
 			var data = $.param({
